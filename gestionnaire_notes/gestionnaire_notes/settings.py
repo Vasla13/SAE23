ac25a6546ba2998cc82e019f657d989feae053a8
Vasla13 <148783329+Vasla13@users.noreply.github.com>
2024-05-23 18:39:55 +0200

    Update settings.py

--- gestionnaire_notes/gestionnaire_notes/settings.py
+++ gestionnaire_notes/gestionnaire_notes/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'gestionnaire_notes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestionnaire_notes',
+        'USER': 'gestionnaire_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
