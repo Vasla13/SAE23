5ebcd88b4e002406c0009f05dc51dd9de2975684
Vasla13 <eripet05@hotmail.com>
2024-05-24 10:25:52 +0200

    V3.2.1

--- gestionnaire_notes/main/views.py
+++ gestionnaire_notes/main/views.py
@@ -7,6 +7,7 @@ from io import BytesIO
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 import csv
+from io import BytesIO
 
 def render_to_pdf(template_src, context_dict):
     template = get_template(template_src)
